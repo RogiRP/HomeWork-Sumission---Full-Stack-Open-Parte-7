1205bbe3aecae71c7df181c7fb3cbd01789b4b1b
roger_leo28hotmail.com <roger_leo28@hotmail.com>
2026-06-26 22:13:33 -0600

    7.2: Anécdotas y Hooks paso 2 -Completado

--- routed-anecdotes/src/components/CreateNew.jsx
+++ routed-anecdotes/src/components/CreateNew.jsx
@@ -13,6 +13,12 @@ const CreateNew = ({ addAnecdote }) => {
     navigate('/')
   }
 
+  const handleReset = () => {
+    content.reset()
+    author.reset()
+    info.reset()
+  }
+
   return (
     <div>
       <h2>create a new anecdote</h2>
@@ -30,6 +36,7 @@ const CreateNew = ({ addAnecdote }) => {
           <input {...info} />
         </div>
         <button>create</button>
+        <button type="button" onClick={handleReset}>reset</button>
       </form>
     </div>
   )
